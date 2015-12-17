965828bce4baf38bb093f6f91b5f00e160e7f7e8
Pnathan22 <nathan.proye@hotmail.com>
2015-12-17 12:45:41 +0100

    Update Sequence.cpp

--- Sequence.cpp
+++ Sequence.cpp
@@ -23,7 +23,20 @@ string Sequence::getDescription()
 {
 	return description;
 }	
+void Sequence::setName(string name)
+{	
+	name = name;
+}
 
+void Sequence::SetLenght(int lenght)
+{
+	lenght=lenght
+}
+
+void Sequence::SetDescription(string description)
+{
+	description=description
+}
 int Sequence::compareTo(Sequence seq)
 {
 }
